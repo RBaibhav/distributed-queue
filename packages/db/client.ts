639066f678760bdfc53be2a65f25d3e20409628f
Baibhav Raj <r.baibhav2023@gmail.com>
2026-08-08 21:19:12 +0000

    init

---
+++ packages/db/client.ts
@@ -0,0 +1,19 @@
+import { config as loadEnv } from "dotenv";
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+loadEnv({ path: new URL(".env", import.meta.url) });
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
